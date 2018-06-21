ad2ea8d6a58f250c2465e18a19787979b37a0e32
maltsev <arelav90@mail.ru>
2018-06-21 16:34:31 +0300

    some args

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,3 +1,19 @@
+import whois
+import argparse
+
+def generate_parser():
+    parser = argparse.ArgumentParser(
+        description='Sites checker')
+    parser.add_argument(
+        '-f',
+        '--file',
+        type=str,
+        metavar='<path_to_file>',
+        required=True,
+        help='full path to file with list of url\'s')
+    return parser
+
+
 def load_urls4check(path):
     try:
         check_list = open(path, 'r')
@@ -13,4 +29,3 @@ def get_domain_expiration_date(domain_name):
     pass
 
 if __name__ == '__main__':
-    pass
